d1641e352c7c2bb8dbcb7ffc0d4282da66868903
Fa Kachi <45835081+wyenelle@users.noreply.github.com>
2022-07-14 21:48:35 +0000

    baddest!!!

--- src/assignment.js
+++ src/assignment.js
@@ -14,9 +14,9 @@ const assignment = {};
  * @returns number the sum of the numbers
  */
 function sumOfNumbers(arrayOfNumbers) {
-    return arrayOfNumbers.length;
+    return arrayOfNumbers.reduce((a,b) => a + b , 0)
 }
-// assignment.sumOfNumbers = sumOfNumbers;
+ assignment.sumOfNumbers = sumOfNumbers;
 
 /**
  * Challenge - 2
@@ -28,9 +28,10 @@ function sumOfNumbers(arrayOfNumbers) {
  * @returns number the count of even numbers
  */
 function countEvenNumbers(arrayOfNumbers) {
-    return arrayOfNumbers.length;
+    let result = arrayOfNumbers.filter(x => x % 2 == 0)
+  return result.length
 }
-// assignment.countEvenNumbers = countEvenNumbers;
+ assignment.countEvenNumbers = countEvenNumbers;
 
 /**
  * Challenge - 3
